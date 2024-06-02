acfc832ec0fe05669ade8d71c30306a1b0635153
Connor Belk <connor.belk@gmail.com>
2024-06-02 01:14:08 -0400

    API POST route created to verify if it works.

--- app/api/addUrl/route.ts
+++ app/api/addUrl/route.ts
@@ -1,19 +1,8 @@
-import { NextResponse, NextRequest } from "next/server";
-import { prisma } from "@/lib/prisma";
 import { nanoid } from "nanoid";
 
-const response = {
-  id: nanoid(25),
-  message: "URL added successfully",
-  otherMessage: "lol",
-  userId: nanoid(50),
-  url: "https://google.com",
-  shortUrl: `shrt.nr/${nanoid(8)}`,
-  createdAt: new Date(),
-  hits: 0,
-};
-const status = 200;
+export async function POST(req: Request) {
+  const { url } = await req.json();
+  const shortUrl = `${process.env.HOSTNAME}/${nanoid(8)}`;
 
-export async function GET(res: NextResponse, req: NextRequest) {
-  return NextResponse.json(response, { status: status });
+  return Response.json({ url: url, shortUrl: shortUrl }, { status: 200 });
 }
