d9c1282185be82db275d30fd63340ea912493f63
Connor Belk <connor.belk@gmail.com>
2024-06-06 18:13:18 -0400

    added expiry dates for urls: for now all expire after 30 days

--- app/api/addUrl/route.ts
+++ app/api/addUrl/route.ts
@@ -3,6 +3,8 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest } from "next/server";
 import { auth } from "@/auth";
 
+const ONE_DAY = 1000 * 60 * 60 * 24;
+
 export async function POST(req: NextRequest) {
   const { url, customTailEnd } = await req.json();
   if (!url) return new Response("invalid url", { status: 400 });
@@ -32,11 +34,14 @@ export async function POST(req: NextRequest) {
     shortUrl = `${req.nextUrl.host}/api/${nanoid(8)}`;
   }
 
+  let expiryDate = new Date(Date.now() + ONE_DAY * 30); // 30 days
+
   await prisma.redirectLinks.create({
     data: {
       shortUrl: shortUrl,
       redirectTo: url,
       ownerId: session?.user?.id || null,
+      expiresAt: expiryDate,
     },
   });
 
