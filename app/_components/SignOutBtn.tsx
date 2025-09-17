7c952627562734a2cd4c0aee3bd1b925c0ee075e
Connor Belk <connor.belk@proton.me>
2025-09-17 16:10:24 -0400

    Added GoogleAuth account selection to signIn functions, and added homepage redirect to SignOut button.

--- app/_components/SignOutBtn.tsx
+++ app/_components/SignOutBtn.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+// import { sign } from "crypto";
 import { signOut } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 const SignOutBtn = () => {
   return (
     <button
       className="px-4 py-2 rounded-lg bg-slate-700 text-white"
-      onClick={() => signOut()}
+      onClick={() => signOut({ callbackUrl: "/" })}
     >
       Sign Out
     </button>
